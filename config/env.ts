f3a22a45bddacca7860d3d7e483c35b0e23e446f
linkdrone <fengwanqing2014@gmail.com>
2024-01-06 17:35:59 +0800

    chore: add env log

--- config/env.ts
+++ config/env.ts
@@ -3,13 +3,17 @@ export const appMainnet = Boolean(
 )
 
 export const thegraphBaseUrl = process.env.NEXT_PUBLIC_THEGRAPH_BASE_URL!
+console.log('Log env thegraphBaseUrl:', thegraphBaseUrl)
 
 export const submitterBaseUrl = process.env.NEXT_PUBLIC_SUBMITTER_BASE_URL!
+console.log('Log env submitterBaseUrl:', submitterBaseUrl)
 
 export const openapiBaseUrl = process.env.NEXT_PUBLIC_OPENAPI_BASE_URL!
+console.log('Log env openapiBaseUrl:', openapiBaseUrl)
 
 export const bountyWhitelistAddresses = (
   process.env.NEXT_PUBLIC_BOUNTY_WHITELIST_ADDRESSES || ''
 )
   .split(',')
   .filter((item) => item.startsWith('0x'))
+console.log('Log env bountyWhitelistAddresses:', bountyWhitelistAddresses)
